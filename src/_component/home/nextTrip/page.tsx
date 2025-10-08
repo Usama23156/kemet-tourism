dcdc169bdadf3c96610c3be8bdbc1b68f53ead34
Usama23156 <usamaelkady25@gmail.com>
2025-10-08 07:38:03 +0300

    status update

--- src/_component/home/nextTrip/page.tsx
+++ src/_component/home/nextTrip/page.tsx
@@ -23,8 +23,8 @@ function page() {
            <img data-aos="fade-right" src="/Why-us-Statue.png" alt="" className=''/>
            <p data-aos="fade-right" className='absolute text-[#b49e09] left-2/7 md:left-1/4 bottom-64 lg:top-1/12 text-base md:text-lg lg:text-xl hover:scale-110 duration-initial transition-transform '>TRUST&CARE </p>
            <p data-aos="fade-left" className='absolute text-[#b49e09] md:right-36 right-64 bottom-64 lg:bottom-80 w-24 md:w-auto text-base md:text-lg lg:text-xl hover:scale-110 duration-initial transition-transform '>AUTHENTIC EXPERIENCES </p>
-           <p data-aos="fade-right" className='absolute text-[#b49e09]  left-2/7 md:left-1/6  bottom-20 w-24 md:w-auto text-base md:text-lg lg:text-xl hover:scale-110 duration-initial transition-transform '>LUXURY ESCAPES</p>
-           <p data-aos="fade-left" className='absolute text-[#b49e09]  right-56 md:right-40 lg:right-52 bottom-20 lg:bottom-20 w-20 md:w-auto text-base md:text-lg lg:text-xl hover:scale-110 duration-initial transition-transform '>LOCAL EXPERTS </p>
+           <p data-aos="fade-right" className='absolute text-[#b49e09]  left-2/7 md:left-1/6 bottom-6 md:bottom-20 w-24 md:w-auto text-base md:text-lg lg:text-xl hover:scale-110 duration-initial transition-transform '>LUXURY ESCAPES</p>
+           <p data-aos="fade-left" className='absolute text-[#b49e09]  right-60 md:right-40 lg:right-52 bottom-6 md:bottom-20 lg:bottom-20 w-20 md:w-auto text-base md:text-lg lg:text-xl hover:scale-110 duration-initial transition-transform '>LOCAL EXPERTS </p>
            <p data-aos="fade-up" className='absolute text-[#b49e09] left-96 -bottom-12 md:-bottom-16 lg:-bottom-20 md:left-2/5 text-base md:text-lg lg:text-xl hover:scale-110 duration-initial transition-transform '>COMPETITIVE PRICING </p>
            </div>
         </div>
--- src/_component/home/sail/page.tsx
+++ src/_component/home/sail/page.tsx
@@ -17,9 +17,9 @@ function page() {
         <img  data-aos="fade-right" src="/Sail-into-1.2.png" alt="" className='lg:max-w-xl md:max-w-lg max-w-80'/>
       </div>
         <div className='flex justify-center items-center overflow-x-hidden '>
-        <img data-aos="fade-left"  src="/last-round-trip-3.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute '/>
-        <img data-aos="fade-up"  src="/last-round-trip-4.png" alt="" className='lg:max-w-2xl md:max-w-lg '/>
-        <img data-aos="fade-right"  src="/last-Round-Trip-2.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute min-w-56 '/>
+        <img data-aos="fade-up"  src="/last-round-trip-3.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute '/>
+        <img data-aos="fade-right"  src="/last-round-trip-4.png" alt="" className='lg:max-w-2xl md:max-w-lg '/>
+        <img data-aos="fade-up"  src="/last-Round-Trip-2.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute min-w-56 '/>
         </div>
         <p  data-aos="fade-up" className={`lg:text-4xl sm:text-2xl md:text-4xl text-xl max-w-3xl text-center uppercase font-extrabold ${CinzelFont.className}`}>The Nile carries more than water; it carries Egypt’s soul.</p>
         <Map/>
