458d396c48b08f81d98deb9cf5ff6eaf5cebaa61
Usama23156 <usamaelkady25@gmail.com>
2025-10-08 08:09:23 +0300

    last update

--- src/_component/home/sail/page.tsx
+++ src/_component/home/sail/page.tsx
@@ -16,14 +16,14 @@ function page() {
         <img  data-aos="fade-right" src="/Sail-into-1.1.png" alt="" className='lg:max-w-xl md:max-w-lg max-w-80'/>
         <img  data-aos="fade-right" src="/Sail-into-1.2.png" alt="" className='lg:max-w-xl md:max-w-lg max-w-80'/>
       </div>
-        <div className='flex justify-center items-center overflow-x-hidden '>
+        <div className='flex justify-center items-center  '>
         <img data-aos="fade-up"  src="/last-round-trip-3.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute '/>
         <img data-aos="fade-right"  src="/last-round-trip-4.png" alt="" className='lg:max-w-2xl md:max-w-lg '/>
         <img data-aos="fade-up"  src="/last-Round-Trip-2.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute min-w-56 '/>
         </div>
         <p  data-aos="fade-up" className={`lg:text-4xl sm:text-2xl md:text-4xl text-xl max-w-3xl text-center uppercase font-extrabold ${CinzelFont.className}`}>The Nile carries more than water; it carries Egypt’s soul.</p>
         <Map/>
-        <div className='flex justify-center items-center overflow-x-hidden'>
+        <div className='flex justify-center items-center'>
           <img data-aos="fade-right" src="/Triangles-1.png" alt="" className='lg:max-w-2xl md:max-w-xl max-w-96 absolute' />
           <img data-aos="fade-up" src="/Triangles-2.png" alt="" className='lg:max-w-2xl md:max-w-xl max-w-96 ' />
           <img data-aos="fade-right" src="/Triangles-3.png" alt="" className='lg:max-w-2xl md:max-w-xl max-w-96 absolute ' />
