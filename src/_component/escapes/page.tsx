b5380a46ac045f8534f29b0e10e8d61942840604
Usama23156 <usamaelkady25@gmail.com>
2025-12-07 05:03:38 +0200

    last escape

--- src/_component/escapes/page.tsx
+++ src/_component/escapes/page.tsx
@@ -6,7 +6,7 @@ function page() {
        <h3 className='font-bold text-center text-3xl'>Escapes Page</h3>
        <div>
         <div className='max-w-3xl flex flex gap-4 mx-auto my-10 p-4'>
-        <img src="/IMG_3426 2.jpg" alt="" width={200} height={100} />
+        <img src="/IMG_3426 2.jpg" alt="" className='w-36' />
         <div>
             <h4>nile scapes</h4>
             <p>Discover the serene beauty .</p>
@@ -20,7 +20,7 @@ function page() {
             <p>Discover the serene beauty .</p>
             <p>Discover the serene beauty of our Nile Escapes, where tranquility meets adventure along the majestic Nile River. Immerse yourself in luxurious accommodations, breathtaking landscapes, and unforgettable experiences that capture the essence of this iconic waterway. Whether you're seeking a romantic getaway or a family adventure, our Nile Escapes offer the perfect blend of relaxation and exploration.</p>
         </div>
-        <img src="/IMG_3426 2.jpg" alt="" width={200} height={100}/>
+        <img src="/IMG_3426 2.jpg" alt="" className='w-36'/>
         </div>
 
        </div>
