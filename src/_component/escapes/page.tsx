bac5b04ef06f931578956686abb1ec738c569bec
Usama23156 <usamaelkady25@gmail.com>
2025-12-07 16:31:55 +0200

    escap

--- src/_component/escapes/page.tsx
+++ src/_component/escapes/page.tsx
@@ -22,7 +22,14 @@ function page() {
         </div>
         <img src="/IMG_3426 2.jpg" alt="" className='w-36'/>
         </div>
-
+         <div className='max-w-3xl flex flex gap-4 mx-auto my-10 p-4'>
+        <img src="/IMG_3426 2.jpg" alt="" className='w-36' />
+        <div>
+            <h4>nile scapes</h4>
+            <p>Discover the serene beauty .</p>
+            <p>Discover the serene beauty of our Nile Escapes, where tranquility meets adventure along the majestic Nile River. Immerse yourself in luxurious accommodations, breathtaking landscapes, and unforgettable experiences that capture the essence of this iconic waterway. Whether you're seeking a romantic getaway or a family adventure, our Nile Escapes offer the perfect blend of relaxation and exploration.</p>
+        </div>
+        </div>
        </div>
     </div>
   )
