a4fe4e109efaca3292d47dae2ff9053f693902ba
Usama23156 <usamaelkady25@gmail.com>
2025-12-16 05:05:04 +0200

    sail

--- src/_component/home/sail/page.tsx
+++ src/_component/home/sail/page.tsx
@@ -22,17 +22,11 @@ function page() {
         <img data-aos="fade-right" src="/last-round-trip-4.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute'/>
         <img data-aos="fade-up" src="/last-Round-Trip-2.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute min-w-56 '/> */}
         <img data-aos="fade-up"  src="/New-idea-round-trip2.png" alt="" className='lg:max-w-4xl md:max-w-xl  min-w-80 '/>
-        <img   src="/last-round-trip-4.png" alt="" className='lg:max-w-2xl md:max-w-lg absolute animate-path'
-                    transform="matrix(0.9791300296783447,0,0,0.9791300296783447,400,179)"
-                    strokeLinejoin="miter"
-                    fillOpacity="0"
-                    pathLength="1"
-                    strokeMiterlimit="4"
-                    stroke="#b49e09"
-                    strokeOpacity="1"
-                    strokeWidth="8"
-                    d="M253,-161 C253,-161 -284.78900146484375,-201.4600067138672 -376,-21 C-469,163 67.62300109863281,174.2100067138672 256,121 C564,34 250.82899475097656,-141.6929931640625 19.10700035095215,-116.93599700927734"
-                />
+        <img
+    src="/last-round-trip-4.png"
+    alt="Description of the image"
+    className="lg:max-w-2xl md:max-w-lg absolute animate-path"
+/>
         </div>
         <p  data-aos="fade-up" className={`lg:text-4xl sm:text-2xl md:text-4xl text-xl max-w-3xl text-center uppercase font-extrabold ${CinzelFont.className}`}>The Nile carries more than water; it carries Egypt’s soul.</p>
         <Map/>
